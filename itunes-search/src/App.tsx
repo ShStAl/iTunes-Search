c6e332fb1c8973c97af344d7beb3851018d2c03e
Stas <128316028+ShStAl@users.noreply.github.com>
2024-03-27 11:01:36 +0300

    Created React+TypeScript+Tailwind project

---
+++ itunes-search/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen w-full flex justify-center items-center">
+        <h1 className="text-fuchsia-500 text-5xl font-bold">Itunes Search</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
