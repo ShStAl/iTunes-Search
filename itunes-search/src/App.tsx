858ddf8326dc2d5943ca65f544bc1f07a072f818
Stas <128316028+ShStAl@users.noreply.github.com>
2024-03-27 16:04:12 +0300

    Add TanStack query
    Removed search button, request completes on type in input field

--- itunes-search/src/App.tsx
+++ itunes-search/src/App.tsx
@@ -1,14 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios"
 import { useState } from "react";
 
 function App() {
 
-  const [searchTerm, setSearchTerm] = useState('');
+  const [search, setSearch] = useState('');
 
-  const getList = async () => {
-    const data = await axios.get(`https://itunes.apple.com/search?term=${searchTerm}&media=music`);
-    console.log(data)
-  };
+  // const getList = async () => {
+  //   return await axios.get(`https://itunes.apple.com/search?term=${search}&media=music`);
+  // };
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['search', search],
+    queryFn: async () => {
+      console.log('fetching')
+      // fetch(`https://itunes.apple.com/search?term=${search}&media=music`)
+      return await axios.get(`https://655c7e1525b76d9884fd6084.mockapi.io/tweets`)
+    }
+  });
+
 
   return (
     <>
@@ -22,20 +32,22 @@ function App() {
               <path strokeLinecap="round" strokeLinejoin="round" d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
             </svg>
           </span>
-          <input 
-            type="email" 
-            placeholder="Search" 
-            value={searchTerm}
-            onChange={(e) => {setSearchTerm(e.target.value)}}
-            className="block w-full py-2.5 text-gray-700 placeholder-gray-400/70 bg-white pl-11 pr-5 outline-none" 
+          <input
+            type="email"
+            placeholder="Search"
+            value={search}
+            onChange={(e) => { setSearch(e.target.value) }}
+            className="block w-full py-2.5 text-gray-700 placeholder-gray-400/70 bg-white pl-11 pr-5 outline-none"
           />
-          <button 
-            onClick={getList} 
-            className="mx-2 bg-gray-200 w-20 h-8 rounded-lg hover:bg-fuchsia-500/60 hover:text-white active:scale-90 active:bg-fuchsia-700 transition-all"
-          >
-            Go
-          </button>
         </div>
+        {isLoading && <p>Loading...</p>}
+        {data && data.data.map((item) => {
+          return (
+            <div key={item.id} className="bg-slate-500">
+              {item.text}
+            </div>
+          )
+        })}
       </main>
     </>
   )
